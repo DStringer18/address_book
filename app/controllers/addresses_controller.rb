9024fa2563eda7cbade1e27d3a889ce82333f3b8
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-09-17 14:21:14 +0200

    trying to fix edit link

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,29 @@
 class AddressesController < ApplicationController
 
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:contact_id])
+    @address = @contact.addresses.find(params[:id])
+  end
+
   def create
     @contact = Contact.find(params[:contact_id])
     @address = @contact.addresses.create(address_params)
     redirect_to contact_path(@contact)
+  end
+
+  def update
+    @contact = Contact.find(params[:contact_id])
+    @address = @contact.addresses.create(address_params)
+
+    if @address.update(address_params)
+      redirect_to @address
+    else
+      render 'edit'
+    end
   end
 
   def destroy
