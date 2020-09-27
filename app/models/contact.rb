dc2a41b4543962956124448303e23bebdaecf9f5
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-09-27 12:11:17 +0200

    updating style and seed file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,6 @@ class Contact < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :phone_numbers, dependent: :destroy
   has_many :emails, dependent: :destroy
+  accepts_nested_attributes_for :addresses, :emails, :phone_numbers
   validates :first_name, :last_name, presence: true
 end
